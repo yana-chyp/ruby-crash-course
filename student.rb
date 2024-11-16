dc493e0961276e0bc213fca762105cb2260b57b5
yana-chyp <yana.chyp@lnu.edu.ua>
2024-11-16 23:28:55 +0200

    Add files via upload

--- student.rb
+++ student.rb
@@ -10,19 +10,17 @@ class Student
     @surname = surname
     @name = name
     begin
-      # Date.valid_date!(date_of_birth)
-      valid_date_of_birth!(date_of_birth)
+      self.class.valid_date_of_birth!(date_of_birth)
       @date_of_birth = date_of_birth
-    # rescue ArgumentError => e
-    #   puts e
-    #   @date_of_birth = Date.new(2004, 11, 18)
+    rescue ArgumentError => e
+      puts e
+      @date_of_birth = Date.new(2004, 11, 18)
     end
-
-    if !present?(self) then @@students << self end
+    unless self.class.present?(self) then @@students << self end
     
   end
 
-  def valid_date_of_birth!(date_of_birth)
+  def self.valid_date_of_birth!(date_of_birth)
     if date_of_birth > Date.today
       raise ArgumentError, 'Date of birth cannot be in the future'
     end
@@ -32,7 +30,7 @@ class Student
     return @surname == student.surname && @name == student.name && @date_of_birth == student.date_of_birth
   end
 
-  def present?(student)
+  def self.present?(student)
     @@students.each {|other_student| 
       if other_student.equal?(student) then return true end}
       return false
@@ -47,19 +45,19 @@ class Student
     end
   end
 
-  def add_student(student)
-    if !present?(student) then @@students << student end
+  def self.add_student(student)
+    unless Student.present?(student) then @@students << student end
   end
 
-  def remove_student(student)
-    if present?(student) then @@students.delete(student) end
+  def self.remove_student(student)
+    if Student.present?(student) then @@students.delete(student) end
   end
 
-  def get_students_by_age(age)
+  def self.get_students_by_age(age)
     @@students.select {|student| student.calculate_age == age}
   end
 
-  def get_students_by_name(name)
+  def self.get_students_by_name(name)
     @@students.select {|student| student.name == name}
   end
 
